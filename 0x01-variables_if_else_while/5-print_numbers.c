b2c57b76050c261369ed19ffbafd44953206987c
Chukwuderah <pleasantchukwu@gmail.com>
2023-06-19 16:08:21 -0700

    Single numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int e;
+
+	for (e = 0; e < 10; e++)
+		printf("%d", e);
+	putchar('\n');
+	return (0);
+}
