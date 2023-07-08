6ee4d5d70c5796c15c4ce5b23bf6c43377240805
Chukwuderah <pleasantchukwu@gmail.com>
2023-07-07 20:00:47 -0700

    static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - a function that checks for lowercase characters
+ * @c: character to be checked
+ * Return: 1 if c is in lowercase, otherwise 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the values of an integer
+ * @n: the int to be checked
+ * Return: value of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
