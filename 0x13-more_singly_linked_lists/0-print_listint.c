c8c5648b04b7b87134fc5ad7458491b9cda7c789
Chukwuderah <pleasantchukwu@gmail.com>
2023-08-01 18:10:32 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t number = 0;
+	size_t numb = 0;
 
 	while (h)
 
 	{
 		printf("%d\n", h->n);
-		number++;
+		numb++;
 		h = h->next;
 	}
 
-	return (number);
+	return (numb);
 }
