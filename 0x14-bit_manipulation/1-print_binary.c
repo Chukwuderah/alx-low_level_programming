fad6e6bb0d9dc4a3cf8f21ba51865d89042a784e
Chukwuderah <pleasantchukwu@gmail.com>
2023-09-28 11:57:26 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: binary number to be printed
+ */
+
+void print_binary(unsigned long int n)
+{
+	int a, c = 0;
+	unsigned long int crt;
+
+	for (a = 63; a >= 0; a--)
+	{
+		crt = n >> a;
+
+		if (crt & 1)
+		{
+			_putchar('1');
+			c++;
+		}
+
+		else if (c)
+			_putchar('0');
+	}
+
+	if (!c)
+		_putchar('0');
+}
